1719699e5b45f9075288b27e7f73ff163f773051
amyhuang190 <amy.huang@uga.edu>
2025-12-02 15:57:01 -0500

    fix

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # final_project_database
 
-# https://www.kaggle.com/datasets/rohiteng/amazon-sales-dataset
-
 # pip install pandas sqlalchemy pymysql
 # pip install cryptography
--- final_project_database/p2_starter_code/target/classes/database_setup.sql
+++ final_project_database/p2_starter_code/target/classes/database_setup.sql
@@ -4,3 +4,16 @@ create database if not exists cs4370_final_libary;
 -- Use the created database.
 use cs4370_final_libary;
 
+--user table
+create table if not exists user (
+    userId int auto_increment primary key,
+    username varchar(255) not null unique,
+    password varchar(255) not null,
+    firstName varchar(255) not null,
+    lastName varchar(255) not null
+    -- primary key (userId),
+    -- unique (username),
+    -- constraint userName_min_length check (char_length(trim(username)) >= 2),
+    -- constraint firstName_min_length check (char_length(trim(firstName)) >= 2),
+    -- constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
+);
