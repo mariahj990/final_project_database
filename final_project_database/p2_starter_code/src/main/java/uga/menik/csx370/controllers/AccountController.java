3e796560f5d2cfa6cc02adfdef117c900ac0356d
amyhuang190 <amy.huang@uga.edu>
2025-12-03 14:31:19 -0500

    Account page

--- final_project_database/p2_starter_code/src/main/java/uga/menik/csx370/controllers/AccountController.java
+++ final_project_database/p2_starter_code/src/main/java/uga/menik/csx370/controllers/AccountController.java
@@ -1,15 +1,41 @@
 package uga.menik.csx370.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import uga.menik.csx370.models.User;
+import uga.menik.csx370.services.AccountService;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     @GetMapping
     public ModelAndView webpage() {
         // posts_page is a mustache template from src/main/resources/templates.
         // ModelAndView class enables initializing one and populating placeholders
         // in the template using Java objects assigned to named properties.
         ModelAndView mv = new ModelAndView("account_page");
+        System.out.println("Trying to view account page");
+        // Get user
+
+        User user = accountService.getCurrentUser();
+        String name = user.getFirstName() +  " " + user.getLastName();
+        System.out.println("name: " + name);
+
+        // Add users to the model
+        mv.addObject("name", name);
+        mv.addObject("user", user);
         return mv;
     }
 }
--- final_project_database/p2_starter_code/src/main/java/uga/menik/csx370/services/AccountService.java
+++ final_project_database/p2_starter_code/src/main/java/uga/menik/csx370/services/AccountService.java
@@ -1,5 +1,58 @@
 package uga.menik.csx370.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uga.menik.csx370.models.User;
+
+/*
+ * This service contains Account related functions. 
+ */
+@Service
 public class AccountService {
-    
+    private final DataSource dataSource;
+    private final UserService userService;
+
+    @Autowired
+    public AccountService(DataSource datasource, UserService userService) {
+        this.dataSource = datasource;
+        this.userService = userService;
+    } //BookmarksService
+
+    public User getCurrentUser() {
+        String userId = userService.getLoggedInUser().getUserId();
+        System.out.println("getCurrentUser: " + userId);
+        
+        // SQL query to get all users
+        final String getUserSql = "SELECT u.userId, u.firstName, u.lastName FROM user u WHERE u.userID = ?";
+
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(getUserSql)) {
+            stmt.setString(1, userId);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    String firstName = rs.getString("firstName");
+                    String lastName = rs.getString("lastName");
+                    // Initialize and retain the logged in user.
+                    User loggedInUser = new User(userId, firstName, lastName);
+                    return loggedInUser;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        System.out.println("User not found.");
+        return null;
+
+    }
+
+
 }
