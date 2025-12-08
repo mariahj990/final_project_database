fcc92a9aaebb690c269a2037898f016f2147ee48
ecw70574 <131731011+ecw70574@users.noreply.github.com>
2025-12-08 05:17:48 -0500

    trying to have springboot automatically run database population scripts on app startup

---
+++ final_project_database/p2_starter_code/src/main/java/uga/menik/csx370/PopulateDatabase.java
@@ -0,0 +1,28 @@
+/**
+Copyright (c) 2024 Sami Menik, PhD. All rights reserved.
+
+This is a project developed by Dr. Menik to give the students an opportunity to apply database concepts learned in the class in a real world project. Permission is granted to host a running version of this software and to use images or videos of this work solely for the purpose of demonstrating the work to potential employers. Any form of reproduction, distribution, or transmission of the software's source code, in part or whole, without the prior written consent of the copyright owner, is strictly prohibited.
+*/
+package uga.menik.csx370;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PopulateDatabaseRunner implements CommandLineRunner {
+
+    // runs after Spring Boot starts
+    // script to populate database 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Running csv loader script...");
+
+        // call python script to load csvs
+        ProcessBuilder pb = new ProcessBuilder("python", "p2_starter_code/src/main/resources/import_book_data.py");
+        pb.inheritIO(); // prints output to console
+        Process process = pb.start();
+        int exitCode = process.waitFor();
+        System.out.println("Python script finished with exit code: " + exitCode);
+    }
+}
