268aea0a12294874a07fe8bc00b87a5355ac0d65
ecw70574 <131731011+ecw70574@users.noreply.github.com>
2025-12-07 20:48:30 -0500

    making has_wishlisted true

--- final_project_database/p2_starter_code/target/classes/import_book_data.py
+++ final_project_database/p2_starter_code/target/classes/import_book_data.py
@@ -13,21 +13,21 @@ engine = create_engine("mysql+pymysql://root:mysqlpass@localhost:33306/cs4370_fi
 # Users
 users = pd.read_csv("datasource_csvs/users.csv")
 print(users.head())
-users.to_sql("user", engine, if_exists='append', index=False)
+users.to_sql("user", engine, if_exists='replace', index=False)
 
 # Books
 books = pd.read_csv("datasource_csvs/books.csv")
 print(books.head())
-books.to_sql("book", engine, if_exists='append', index=False)
+books.to_sql("book", engine, if_exists='replace', index=False)
 
 # Ratings
 ratings = pd.read_csv("datasource_csvs/ratings.csv")
 print(ratings.head())
-ratings.to_sql("ratings", engine, if_exists='append', index=False)
+ratings.to_sql("ratings", engine, if_exists='replace', index=False)
 
 # CSV coming from external table: to_read 
 history = pd.read_csv(history_csv_path)
 # Print the first 5 rows of the DataFrame
 print(history.head())
-history.to_sql("history", engine, if_exists='append', index=False)
+history.to_sql("history", engine, if_exists='replace', index=False)
 
